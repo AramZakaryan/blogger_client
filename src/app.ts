761b9c371696336e6383e1e06bcd32e2a0d8f5a1
Aram Zakaryan <aram.zakaryan@n.rich>
2025-05-18 22:55:47 +0400

    new config

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,15 @@
 import express, {Response} from 'express'
 import cors from 'cors'
 import {config} from 'dotenv'
-import path from 'node:path'
+// import path from 'node:path'
 
 export const app = express()
 
 app.use(express.json())
 app.use(cors())
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
+// app.set('views', path.join(__dirname, 'views'));
 
 config()
 
@@ -18,7 +18,7 @@ const version = process.env.VERSION
 app.get('/', (_, res: Response) => {
     res.status(200).json({client_version: version})
 })
-
-app.get('/views', (_, res) => {
-    res.render('index', {})
-})
+//
+// app.get('/views', (_, res) => {
+//     res.render('index', {})
+// })
